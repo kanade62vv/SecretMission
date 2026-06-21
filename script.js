46275fd1a06f30c72e071dc64ca9440301a81ccc
kanade62vv <kanade.sato@outlook.jp>
2026-06-21 14:25:01 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+function checkPassword(){
+    const password=document.getElementById("password").value;
+    const result=document.getElementById("result");
+    if(password==="escape16"){
+        result.innerHTML=`
+        <h2>ACCESS GRANTED</h2>
+
+        <p>
+        
+        MISSION COMPLETE
+        
+        </p>
+
+        <p>
+
+        脱出おめでとうございます。この画面を入口の運営に見せてください
+
+        </p>
+
+        `;
+
+    }
+
+    else{
+        result.innerHTML=`
+
+        <h2>
+
+        ACCESS DENIED
+
+        </h2>
+
+        <p>
+
+        パスワードが違います
+
+        </p>
+        `;
+    }
+
+}
